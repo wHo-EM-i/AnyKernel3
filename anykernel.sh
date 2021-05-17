47571e355b84d3e7fc00214abb5b7ff2e225bcf6
ehtesham01 <ehteshammalik4@gmail.com>
2021-05-17 08:17:52 +0530

    Initial

---
+++ anykernel.sh
@@ -0,0 +1,48 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Beast-lavender-oldcam
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=lavender
+supported.versions=9 - 12
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+
+# init.tuna.rc
+
+# fstab.tuna
+
+# end ramdisk changes
+
+write_boot;
+## end install
+
